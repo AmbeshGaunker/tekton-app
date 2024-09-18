d0654d988d33a2ffa90cb596f5b8a6a34bf5aab6
Ambesh <ambesh.gaunker@turtlemint.com>
2024-09-18 11:21:07 +0530

    failing test case

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, date
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + timedelta(days=4)
-    return next_month - timedelta(days=next_month.day)
+    return next_month + timedelta(days=next_month.day) #changed sign to + from -
 
 result = last_day_of_month(date(2023, 1, 15) )
 print(result)
